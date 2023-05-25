0a54848c92b20f49d02157b4a5ccfa02a4fc2939
Márton Makrai <makrai.hlt@gmail.com>
2023-05-25 14:51:30 +0200

    Debugged: empty sentences, those starting with #

--- train_two_embeds.py
+++ train_two_embeds.py
@@ -7,13 +7,14 @@ import gzip
 import logging
 import os
 import re
+import sys
 
 import gensim
 import pandas as pd
 
 logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S',
-    level=logging.DEBUG)
+    level=logging.INFO)
 
 
 def deglutinate(emtsv_record):
@@ -35,7 +36,7 @@ def get_sentences(file_pattern='2017_2018_2956', deglutinate_=False):
     Yields a series for each sentence.
     """
     files = glob.glob(f'/mnt/permanent/home/ndavid/public_html/Webcorpus2_clean/{file_pattern}*.tsv.gz')
-    for filen in sorted(files, key = os.path.getsize): 
+    for filen in sorted(files, key = os.path.getsize):
         logging.debug(filen)
         with gzip.open(filen, mode='rt') as textwrapper:
             word_records = []
@@ -51,19 +52,23 @@ def get_sentences(file_pattern='2017_2018_2956', deglutinate_=False):
                     sent_df = pd.DataFrame(word_records, columns=columns)
                     word_records = []
                     if deglutinate_:
+                        if sent_df.empty:
+                            logging.warning('empty sentence')
+                            continue
                         yield sent_df.apply(deglutinate, axis=1).sum()
                     else:
                         yield list(sent_df.form)
-                elif line.startswith('#'):
-                    if line.startswith('# text ='):
-                        pass
-                        # logging.debug = line[9:]
+                elif line.startswith('# newdoc id = '):
+                    pass
+                elif line.startswith('# newpar id = '):
+                    pass
+                elif line.startswith('# text = '):
+                    pass
                 else:
                     word_records.append(line.split('\t'))
 
 
-for i in  [3,1,2,0]:
-    file_pattern = f'2017_2018_{i}'#, '2017_2018', '201', '']:
+for file_pattern in ['wiki_', '2017_2018', '201', '']:
     for deglutinate_ in [False, True]:
         deglutinate_humanread = 'deglut' if deglutinate_ else 'vanila'
         logging.info((file_pattern, deglutinate_humanread))
@@ -73,6 +78,7 @@ for i in  [3,1,2,0]:
         if os.path.exists(trained_model_filen):
             logging.info(f'file exists: {trained_model_filen}')
         else:
+            logging.debug(vocab_model_filen)
             if os.path.exists(vocab_model_filen):
                 logging.info(f'loading {vocab_model_filen}')
                 model = gensim.models.Word2Vec.load(vocab_model_filen)
@@ -81,7 +87,7 @@ for i in  [3,1,2,0]:
                 model = gensim.models.Word2Vec(vector_size=300, sg=True)
                 model.build_vocab(corpus_iterable=get_sentences(file_pattern=file_pattern, deglutinate_=deglutinate_))
                 model.save(filen+'-vocab.gensim')
-            logging('training model..')
-            model.train(get_sentences(file_pattern=file_pattern, deglutinate_=deglutinate_), 
+            logging.info('training model..')
+            model.train(get_sentences(file_pattern=file_pattern, deglutinate_=deglutinate_),
                              total_examples=model.corpus_count, epochs=1)
             model.save(f'{filen}.gensim')
