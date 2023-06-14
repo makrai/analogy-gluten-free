96aed47b2cb152c9cf0e525a60f617843981050a
Márton Makrai <makrai.hlt@gmail.com>
2023-06-14 13:49:07 +0200

    Deglut: all compos morphemes separately. Better result on very small corpora. Larger training in progress

--- train_two_embeds.py
+++ train_two_embeds.py
@@ -7,14 +7,13 @@ import gzip
 import logging
 import os
 import re
-import sys
 
 import gensim
 import pandas as pd
 
 logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-    datefmt='%Y-%m-%d:%H:%M:%S',
-    level=logging.INFO)
+                    datefmt='%Y-%m-%d:%H:%M:%S',
+                    level=logging.INFO)
 
 
 def deglutinate(emtsv_record):
@@ -24,9 +23,10 @@ def deglutinate(emtsv_record):
     """
     morphs = re.findall('\[[^/][^\]]*\]', emtsv_record.xpostag)
     # WONTFIX: prefixes (e.g. leg-)
-    morphs = [morph for morph in morphs if morph not in ['[Nom]', '[Punct]']]
+    morphs = [morph for morph in morphs if morph not in ['[Nom]', '[Prs.NDef.3Sg]', '[Punct]']]
+    # [Punct] is dropped, because the typical case  is "pl.", analized as [/Adv|Abbr][Punct]
     if morphs:
-        return [emtsv_record.lemma, "".join(morphs)]
+        return [emtsv_record.lemma] + morphs
     else:
         return [emtsv_record.form]
 
@@ -36,7 +36,7 @@ def get_sentences(file_pattern='2017_2018_2956', deglutinate_=False):
     Yields a series for each sentence.
     """
     files = glob.glob(f'/mnt/permanent/home/ndavid/public_html/Webcorpus2_clean/{file_pattern}*.tsv.gz')
-    for filen in sorted(files, key = os.path.getsize):
+    for filen in sorted(files, key=os.path.getsize):
         logging.debug(filen)
         with gzip.open(filen, mode='rt') as textwrapper:
             word_records = []
@@ -46,7 +46,7 @@ def get_sentences(file_pattern='2017_2018_2956', deglutinate_=False):
                 line = line.strip()
                 if not line:
                     if first_sent_in_file:
-                        columns =  word_records[0]
+                        columns = word_records[0]
                         word_records = word_records[1:]
                         first_sent_in_file = False
                     sent_df = pd.DataFrame(word_records, columns=columns)
@@ -70,7 +70,7 @@ def get_sentences(file_pattern='2017_2018_2956', deglutinate_=False):
 
 for file_pattern in ['2017_2018_2956', '2017_2018_295', '2017_2018_29', 'wiki', '2017_2018_2', '2017_2018', '201', '']:
     for deglutinate_ in [False, True]:
-        deglutinate_humanread = 'deglut' if deglutinate_ else 'vanila'
+        deglutinate_humanread = 'deglutAllSuffSepd' if deglutinate_ else 'vanila'
         logging.info((file_pattern, deglutinate_humanread))
         filen = f'/mnt/permanent/Language/Hungarian/Embed/webcorpus2.0/sgns-300-{file_pattern}-{deglutinate_humanread}'
         vocab_model_filen = f'{filen}-vocab.gensim'
@@ -89,5 +89,5 @@ for file_pattern in ['2017_2018_2956', '2017_2018_295', '2017_2018_29', 'wiki', 
                 model.save(filen+'-vocab.gensim')
             logging.info('training model..')
             model.train(get_sentences(file_pattern=file_pattern, deglutinate_=deglutinate_),
-                             total_examples=model.corpus_count, epochs=1)
+                        total_examples=model.corpus_count, epochs=1)
             model.save(f'{filen}.gensim')
